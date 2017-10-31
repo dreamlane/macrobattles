d67714d1e725c038fb6675b86fbec8851dbdaa69
Benjamin Johnson <bjohnson.lane@gmail.com>
2017-10-30 21:14:50 -0600

    Remove deprecated Townsperson code

--- MacroBattlesServer/main.py
+++ MacroBattlesServer/main.py
@@ -2,7 +2,6 @@
 
 import logging
 import webapp2
-from decisionhandlers import TownspersonHireHandler
 from userhandlers import UserLoginHandler
 from requestutils import BaseHandler
 from gamelogic import addPlayerToWorld
@@ -15,16 +14,6 @@ class RegisterHandler(BaseHandler):
   def post(self):
     self.response.write(UserLoginHandler.handleRegisterRequest(self.request))
 
-class TownspersonListHandler(BaseHandler):
-  def get(self):
-    self.session['townsperson'] = 'called'
-    self.response.write(self.session)
-
-class TownspersonHandler(BaseHandler):
-  def get(self, tp_id):
-    self.session['tp_id'] = tp_id
-    self.response.write(self.session)
-
 class JoinGameHandler(BaseHandler):
   def get(self):
     # TODO, figure out authentication!
@@ -35,7 +24,5 @@ config['webapp2_extras.sessions'] = {'secret_key': 'r7ps9bd6daoc1984shmogogin'}
 app = webapp2.WSGIApplication([
   webapp2.Route(r'/login', handler=LoginHandler, name='login'),
   webapp2.Route(r'/register', handler=RegisterHandler, name='register'),
-  webapp2.Route(r'/townsperson', handler=TownspersonListHandler, name='tp-list'),
-  webapp2.Route(r'/townsperson/<tp_id:\d+>', handler=TownspersonHandler, name='tp'),
   webapp2.Route(r'/join-game', handler=JoinGameHandler, name='join-game'),
 ], debug=True, config=config)

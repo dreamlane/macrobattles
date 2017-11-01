3ec8732ba3910bb1233eacb41cb1be75cb1aa0c8
Benjamin Johnson <bjohnson.lane@gmail.com>
2017-10-31 21:58:23 -0600

    Add in the unit_constants.py I forgot in the previous commit

---
+++ MacroBattlesServer/unit_constants.py
@@ -0,0 +1,22 @@
+## unit_constants.py
+""" This file defines all of the constant values for units."""
+
+WORKER_KEY = "WORKER"
+SOLDIER_KEY = "SOLDIER"
+
+""" The key-value pairs for types of resources."""
+UNIT_TYPES_INT_MAPPING = {
+  WORKER_KEY: 0,
+  SOLDIER_KEY: 1
+}
+
+UNIT_COSTS = {
+  WORKER_KEY: 100,
+  SOLDIER_KEY: 100,
+}
+
+UNIT_BASE_HEALTH ={
+  WORKER_KEY: 100,
+  SOLDIER_KEY: 100,
+}
+

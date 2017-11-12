5b421b41492f3e575d536731516079aa71e626d9
Benjamin Johnson <bjohnson.lane@gmail.com>
2017-11-11 20:31:53 -0700

    Add begining support for drawing a map and camera movement.

--- MacroBattlesUnity/MacroBattles/Assets/Scripts/GameObjectScripts/MapEngine.cs
+++ MacroBattlesUnity/MacroBattles/Assets/Scripts/GameObjectScripts/MapEngine.cs
@@ -34,8 +34,14 @@ public class MapEngine : MonoBehaviour {
       // Make a new game object for every map tile in the model.
       GameObject tileObject = Instantiate(mapTilePrefab) as GameObject;
       tileObject.transform.parent = mapObject.transform;
+      Renderer renderer = tileObject.GetComponent<Renderer>();
+      float x_pos = tileModel.coordinate_x * renderer.bounds.size.x;
+      float y_pos = tileModel.coordinate_y * renderer.bounds.size.y;
       tileObject.transform.localPosition =
-          new Vector2(tileModel.coordinate_x, tileModel.coordinate_y);
+          new Vector2(x_pos, y_pos);
+      MapTileScript script = tileObject.GetComponent(typeof(MapTileScript)) as MapTileScript;
+      script.x = tileModel.coordinate_x;
+      script.y = tileModel.coordinate_y;
     }
     mapBuilt = true;
   }
